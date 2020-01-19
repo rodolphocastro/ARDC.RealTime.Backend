5c7a73d0ade9a0a7937dffd2d524d579abd3c88e
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-01-19 17:36:39 -0300

    Adicionar Models à API

---
+++ Realtime.Api/Models/BroadcastMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Realtime.Api.Models
+{
+    /// <summary>
+    /// Mensagem de aviso para todos os usuários do Aplicativo.
+    /// </summary>
+    public class BroadcastMessage
+    {
+        /// <summary>
+        /// Identificador único da mensagem.
+        /// </summary>
+        public Guid MessageId { get; set; }
+
+        /// <summary>
+        /// Título da mensagem.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Conteúdo da mensagem.
+        /// </summary>
+        public string Content { get; set; }
+    }
+}
