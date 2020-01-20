407ee5de75a60793fbd0dd92886f8c1436a85461
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-01-19 23:33:00 -0300

    Adicionar os Handlers à injeção de dependência

---
+++ Realtime.Api/Handlers/HandlerServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extensions para registrar os Handlers.
+    /// </summary>
+    public static class HandlerServiceExtensions
+    {
+        /// <summary>
+        /// Adiciona serviços para os Command e Query Handlers do projeto.
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddMediatRHandlers(this IServiceCollection services)
+        {
+            var types = new[]
+            {
+                typeof(Realtime.Api.Handlers.Broadcast.CommandHandler),
+                typeof(Realtime.Api.Handlers.Broadcast.QueryHandler),
+                typeof(Realtime.Api.Handlers.Messages.CommandHandler),
+                typeof(Realtime.Api.Handlers.Messages.QueryHandler)
+            };
+
+            services.AddMediatR(types);
+        }
+    }
+}
--- Realtime.Api/Startup.cs
+++ Realtime.Api/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace Realtime.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddHashStores();
+            services.AddMediatRHandlers();
             services.AddControllers();
         }
 
