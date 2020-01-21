5b8799d12e63f3ec0444472e9f539430ed302bf6
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-01-21 20:58:49 -0300

    Adicionar hubs para Broadcasts e Mensagens

---
+++ Realtime.Api/Hubs/BroadcastHub.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using Realtime.Api.Clients;
+using Realtime.Api.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Realtime.Api.Hubs
+{
+    /// <summary>
+    /// Hub para interação com Broadcasts.
+    /// </summary>
+    public class BroadcastHub : Hub<IBroadcastClient>
+    {
+        public const string HubEndpoint = "/broadcastHub";
+        private readonly ILogger<BroadcastHub> logger;
+
+        public BroadcastHub(ILogger<BroadcastHub> logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task NotifyNewBroadcast(BroadcastMessage broadcast)
+        {
+            await Clients.All.ReceiveNewBroadcast(broadcast);
+        }
+
+        public async Task NotifyDeletedBroadcast(BroadcastMessage broadcast)
+        {
+            await Clients.All.ReceiveDeletedBroadcast(broadcast);
+        }
+
+        public async Task NotifyUpdatedBroadcast(BroadcastMessage broadcast)
+        {
+            await Clients.All.ReceiveUpdatedBroadcast(broadcast);
+        }
+
+    }
+}
