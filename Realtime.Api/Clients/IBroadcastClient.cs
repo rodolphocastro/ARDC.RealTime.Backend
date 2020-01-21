a79067047dd366476f6dafdbd5833b1310858443
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-01-20 23:19:12 -0300

    Adicionar definições para os Clients do SignalR

---
+++ Realtime.Api/Clients/IBroadcastClient.cs
@@ -0,0 +1,30 @@
+﻿using Realtime.Api.Models;
+using System.Threading.Tasks;
+
+namespace Realtime.Api.Clients
+{
+    /// <summary>
+    /// Métodos para Clients dos Broadcasts.
+    /// </summary>
+    public interface IBroadcastClient
+    {
+        /// <summary>
+        /// Recebe um novo broadcast criado no sistema.
+        /// </summary>
+        /// <param name="broadcastMessage">o novo broadcast</param>
+        Task ReceiveNewBroadcast(BroadcastMessage broadcastMessage);
+
+        /// <summary>
+        /// Recebe um broadcast deletado do sistema.
+        /// </summary>
+        /// <param name="broadcastMessage">o broadcast deletado</param>
+        Task ReceiveDeletedBroadcast(BroadcastMessage broadcastMessage);
+
+        /// <summary>
+        /// Recebe um broadcast atualizado no sistema.
+        /// </summary>
+        /// <param name="broadcastMessage">o broadcast atualizado</param>
+        Task ReceiveUpdatedBroadcast(BroadcastMessage broadcastMessage);
+
+    }
+}
---
+++ Realtime.Api/Clients/IMessageClient.cs
@@ -0,0 +1,37 @@
+﻿using Realtime.Api.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Realtime.Api.Clients
+{
+    /// <summary>
+    /// Métodos para Clients das Mensagens.
+    /// </summary>
+    public interface IMessageClient
+    {
+        /// <summary>
+        /// Recebe uma nova mensagem enviada para o sistema.
+        /// </summary>
+        /// <param name="message">a mensagem criada</param>
+        Task ReceiveNewMessage(UserMessage message);
+
+        /// <summary>
+        /// Recebe uma mensagem deletada do sistema.
+        /// </summary>
+        /// <param name="message">a mensagem deletada</param>
+        Task ReceiveDeletedMessage(UserMessage message);
+
+        /// <summary>
+        /// Cria uma nova mensagem no sistema.
+        /// </summary>
+        /// <param name="content">conteudo da mensagem</param>
+        /// <param name="username">nome do usuário</param>
+        Task SendMessage(string content, string username);
+
+        /// <summary>
+        /// Deleta uma mensagem do sistema.
+        /// </summary>
+        /// <param name="messageId">id da mensagem</param>
+        Task DeleteMessage(Guid messageId);
+    }
+}
